57685f70352f0cb524a7de9cea22aac48a8e6db1
Tu (Tony) A. TRAN <tu.tran@Johnnys-Hackintosh.local>
2014-07-25 09:50:17 +0700

    Add function and change project structure

---
+++ AFNetworkingHelper/AFNetworkingHelper.h
@@ -0,0 +1,155 @@
+//
+//  PLISTHelper.h
+//  DallasAFNetworkingHelper
+//
+//  Created by ENCLAVEIT on 7/23/14.
+//  Copyright (c) 2014 ENCLAVEIT. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface AFNetworkingHelper : NSObject
+/** request PList AFHTTP Request Operation
+ @param fileURL :The request object to be loaded asynchronously during execution of the operation.
+ @param successBlock :^(AFHTTPRequestOperation *operation, id responseObject) {}
+ A block object to be executed when the request operation finishes successfully. This block has no return value and takes two arguments: the created request operation and the object created from the response data of request.
+ @param failBlock :^(AFHTTPRequestOperation *operation, NSError *error) {}
+ A block object to be executed when the request operation finishes unsuccessfully, or that finishes successfully, but encountered an error while parsing the response data. This block has no return value and takes two arguments:, the created request operation and the NSError object describing the network or parsing error that occurred.
+ @author: Tony
+ */
++ (void)requestPListAFHTTPRequestOperation:(NSURL *)uploadFileApi
+                              success:(id)successBlock
+                                 fail:(id)failBlock;
+
+/** request JSON AFHTTP Request Operation
+ @param fileURL :The request object to be loaded asynchronously during execution of the operation.
+ @param successBlock :^(AFHTTPRequestOperation *operation, id responseObject) {}
+ A block object to be executed when the request operation finishes successfully. This block has no return value and takes two arguments: the created request operation and the object created from the response data of request.
+ @param failBlock :^(AFHTTPRequestOperation *operation, NSError *error) {}
+ A block object to be executed when the request operation finishes unsuccessfully, or that finishes successfully, but encountered an error while parsing the response data. This block has no return value and takes two arguments:, the created request operation and the NSError object describing the network or parsing error that occurred.
+ @author: William
+ */
++ (void)requestJSONAFHTTPRequestOperation:(NSURL *)fileURL
+                                  success:(id)successBlock
+                                     fail:(id)failBlock;
+
+/** request XML AFHTTP Request Operation
+ @param fileURL :The request object to be loaded asynchronously during execution of the operation.
+ @param successBlock :^(AFHTTPRequestOperation *operation, id responseObject) {}
+ A block object to be executed when the request operation finishes successfully. This block has no return value and takes two arguments: the created request operation and the object created from the response data of request.
+ @param failBlock :^(AFHTTPRequestOperation *operation, NSError *error) {} :
+ A block object to be executed when the request operation finishes unsuccessfully, or that finishes successfully, but encountered an error while parsing the response data. This block has no return value and takes two arguments:, the created request operation and the NSError object describing the network or parsing error that occurred.
+ @author: Violet
+ */
++ (void)requestXMLAFHTTPRequestOperation:(NSURL *)fileURL
+                                 success:(id)successBlock
+                                    fail:(id)failBlock;
+
+/** cancel All HTTP Operations
+ @param method :
+ @param path:
+ @param successBlock :A block object to be executed when the request operation finishes successfully. This block has no return value and takes two arguments: the created request operation and the object created from the response data of request.
+ @param failBlock :A block object to be executed when the request operation finishes unsuccessfully, or that finishes successfully, but encountered an error while parsing the response data. This block has no return value and takes two arguments:, the created request operation and the NSError object describing the network or parsing error that occurred.
+ @author: William
+ */
++ (void)cancelAllHTTPOperations:(NSString *)method path:(NSString *)path;
+
+
+/** POST MULTI-PART REQUEST
+ @param fileURL :The request object to be loaded asynchronously during execution of the operation.
+ @param successBlock :A block object to be executed when the request operation finishes successfully. This block has no return value and takes two arguments: the created request operation and the object created from the response data of request.
+ @param failBlock :A block object to be executed when the request operation finishes unsuccessfully, or that finishes successfully, but encountered an error while parsing the response data. This block has no return value and takes two arguments:, the created request operation and the NSError object describing the network or parsing error that occurred.
+ @author: Tony
+ */
++ (void)multiPartHTTPRequest:(NSString *)fileURL  filePath:(NSURL *)filePath
+                 parameters:(NSDictionary*)parameters
+                    success:(id)successBlock
+                       fail:(id)failBlock;
+
+/** POST-GET REQUEST
+ @param fileURL :The request object to be loaded asynchronously during execution of the operation.
+ @param type : if type == "POST": POST URL-FORM-ENCODED REQUEST
+ type : if type == "GET ": GET REQUEST
+ @param successBlock :A block object to be executed when the request operation finishes successfully. This block has no return value and takes two arguments: the created request operation and the object created from the response data of request.
+ @param failBlock :A block object to be executed when the request operation finishes unsuccessfully, or that finishes successfully, but encountered an error while parsing the response data. This block has no return value and takes two arguments:, the created request operation and the NSError object describing the network or parsing error that occurred.
+ @author: Violet
+ */
++(void)postgetAFHTTPOperation:(NSString *)fileURL parameters:(NSDictionary*)parameters
+                       isPOST:(BOOL *)isPOST
+                      success:(id)successBlock
+                         fail:(id)failBlock;
+
+
+/** CREATING A DOWNLOAD TASK
+ @param completionHandler:^(NSURLResponse *response, NSURL *filePath, NSError *error) {}
+ @param destination:^NSURL *(NSURL *targetPath, NSURLResponse *response) {}
+ destination A block object to be executed in order to determine the destination of the downloaded file. This block takes two arguments, the target path & the server response, and returns the desired file URL of the resulting download. The temporary file used during the download will be automatically deleted after being moved to the returned URL.
+ @param progress:(NSProgress *__autoreleasing *)
+ progress A progress object monitoring the current download progress.
+ @param successBlock:
+ completionHandler A block to be executed when a task finishes. This block has no return value and takes three arguments: the server response, the path of the downloaded file, and the error describing the network or parsing error that occurred, if any.
+ 
+ @param isResume: if isResume == Yes call run download task, else run create download task with resume data
+ @warning If using a background `NSURLSessionConfiguration` on iOS, these blocks will be lost when the app is terminated. Background sessions may prefer to use `-setDownloadTaskDidFinishDownloadingBlock:` to specify the URL for saving the downloaded file, rather than the destination block of this method.
+ @author: Tony
+ */
++ (void)createAFHTTPDownloadTask:(NSURL*)downloadURL isResume:(BOOL)isResume withData:(NSData *)data destination:(id)destination success:(id)successBlock;
+
+
+/** CREATING AN UPLOAD TASK with the specified request for a local file
+ 
+ @param uploadURL A URL to the local file to be uploaded.
+ @param successBlock : ^(NSURLResponse *response, id responseObject, NSError *error) {}
+ A block object to be executed when the task finishes. This block has no return value and takes three arguments: the server response, the response object created by that serializer, and the error that occurred, if any.
+ @param progress :(NSProgress *__autoreleasing *)
+ progress A progress object monitoring the current upload progress.
+ @author: Violet
+ */
+
+/** CREATING AN UPLOAD TASK FOR A MULTI-PART REQUEST, WITH PROGRESS
+ @param successBlock : ^(NSURLResponse *response, id responseObject, NSError *error) {}
+ A block object to be executed when the task finishes. This block has no return value and takes three arguments: the server response, the response object created by that serializer, and the error that occurred, if any.
+ @param progress :(NSProgress *__autoreleasing *)
+ A progress object monitoring the current upload progress.
+ 
+ @author: William
+ */
++ (void)createAFHTTPUploadTask:(NSURL*)uploadURL filePath:(NSURL*)filePath destination:(id)destination success:(id)successBlock;
+
+
+/** CREATING A DATA TASK
+ @param successBlock : ^(NSURLResponse *response, id responseObject, NSError *error) {}
+ completionHandler A block object to be executed when the task finishes. This block has no return value and takes three arguments: the server response, the response object created by that serializer, and the error that occurred, if any.
+ @param progress :(NSProgress *__autoreleasing *)
+ @author: Tony
+ */
++ (void)createDataAFHTTPTask:(NSURL*)uploadURL filePath:(NSString*)filePath success:(id)successBlock;
+
+/** 
+ 
+ */
++ (NSMutableURLRequest*)parametersEncodingwithMethod:(NSString*)method urlString:(NSString*)urlString contentType:(NSString*)contentType parameters:(NSDictionary*)parameters;
+
+/** get SHARED NETWORK REACHABILITY
+ 
+ */
++(void)getShareNetworkStatus;
+
+/** get HTTP Network REACHABILITY
+ 
+ */
++(void)getHTTPNetworkStatus:(NSURL*)URL;
+
+
+/** GET WITH AFHTTPREQUESTOPERATION
+ 
+ */
++(void)getAFHTTPRequestOperation:(NSURL*)url success:(id)successBlock fail:(id)failBlock;
+
+/** BATCH OF OPERATIONS
+ 
+ */
++(void)callBatchOfRequestOperations:(NSString*)url method:(NSString*)method batch:(id)batch filesToUpload:(NSArray *)filesToUpload success:(id)successBlock fail:(id)failBlock;
+
+
+@end
--- AFNetworkingHelper/AFNetworkingHelper.m
+++ AFNetworkingHelper/AFNetworkingHelper.m
@@ -6,20 +6,13 @@
 //  Copyright (c) 2014 ENCLAVEIT. All rights reserved.
 //
 
-#import "PLISTHelper.h"
+#import "AFNetworkingHelper.h"
 
-@implementation PLISTHelper
+@implementation AFNetworkingHelper
 
 
 #pragma mark - HTTP Request Operation Manager
-/** request PList AFHTTP Request Operation
- @param fileURL :The request object to be loaded asynchronously during execution of the operation.
- @param successBlock :^(AFHTTPRequestOperation *operation, id responseObject) {}
-        A block object to be executed when the request operation finishes successfully. This block has no return value and takes two arguments: the created request operation and the object created from the response data of request.
- @param failBlock :^(AFHTTPRequestOperation *operation, NSError *error) {} 
-        A block object to be executed when the request operation finishes unsuccessfully, or that finishes successfully, but encountered an error while parsing the response data. This block has no return value and takes two arguments:, the created request operation and the NSError object describing the network or parsing error that occurred.
- @author:
- */
+
 + (void)requestPListAFHTTPRequestOperation:(NSURL *)fileURL
                               success:(id)successBlock
                                  fail:(id)failBlock
@@ -38,14 +31,7 @@
     [op start];
 }
 
-/** request JSON AFHTTP Request Operation
- @param fileURL :The request object to be loaded asynchronously during execution of the operation.
- @param successBlock :^(AFHTTPRequestOperation *operation, id responseObject) {} 
-        A block object to be executed when the request operation finishes successfully. This block has no return value and takes two arguments: the created request operation and the object created from the response data of request.
- @param failBlock :^(AFHTTPRequestOperation *operation, NSError *error) {} 
-        A block object to be executed when the request operation finishes unsuccessfully, or that finishes successfully, but encountered an error while parsing the response data. This block has no return value and takes two arguments:, the created request operation and the NSError object describing the network or parsing error that occurred.
- @author:
- */
+
 + (void)requestJSONAFHTTPRequestOperation:(NSURL *)fileURL
                                    success:(id)successBlock
                                       fail:(id)failBlock
@@ -64,14 +50,7 @@
     [op start];
 }
 
-/** request XML AFHTTP Request Operation
- @param fileURL :The request object to be loaded asynchronously during execution of the operation.
- @param successBlock :^(AFHTTPRequestOperation *operation, id responseObject) {}
-        A block object to be executed when the request operation finishes successfully. This block has no return value and takes two arguments: the created request operation and the object created from the response data of request.
- @param failBlock :^(AFHTTPRequestOperation *operation, NSError *error) {} :
-        A block object to be executed when the request operation finishes unsuccessfully, or that finishes successfully, but encountered an error while parsing the response data. This block has no return value and takes two arguments:, the created request operation and the NSError object describing the network or parsing error that occurred.
- @author:
- */
+
 + (void)requestXMLAFHTTPRequestOperation:(NSURL *)fileURL
                                    success:(id)successBlock
                                       fail:(id)failBlock
@@ -90,24 +69,11 @@
     [op start];
 }
 
-/** cancel All HTTP Operations
- @param method :
- @param path:
- @param successBlock :A block object to be executed when the request operation finishes successfully. This block has no return value and takes two arguments: the created request operation and the object created from the response data of request.
- @param failBlock :A block object to be executed when the request operation finishes unsuccessfully, or that finishes successfully, but encountered an error while parsing the response data. This block has no return value and takes two arguments:, the created request operation and the NSError object describing the network or parsing error that occurred.
- @author:
- */
 + (void)cancelAllHTTPOperations:(NSString *)method path:(NSString *)path{
     
 }
 
 
-/** POST MULTI-PART REQUEST
- @param fileURL :The request object to be loaded asynchronously during execution of the operation.
- @param successBlock :A block object to be executed when the request operation finishes successfully. This block has no return value and takes two arguments: the created request operation and the object created from the response data of request.
- @param failBlock :A block object to be executed when the request operation finishes unsuccessfully, or that finishes successfully, but encountered an error while parsing the response data. This block has no return value and takes two arguments:, the created request operation and the NSError object describing the network or parsing error that occurred.
- @author:
- */
 +(void)multiPartHTTPRequest:(NSString *)fileURL  filePath:(NSURL *)filePath
                  parameters:(NSDictionary*)parameters
                     success:(id)successBlock
@@ -121,14 +87,6 @@
     [manager POST:fileURL parameters:parameters constructingBodyWithBlock:constructingBody success:successBlock failure:failBlock];
 }
 
-/** POST-GET REQUEST
- @param fileURL :The request object to be loaded asynchronously during execution of the operation.
- @param type : if type == "POST": POST URL-FORM-ENCODED REQUEST
-        type : if type == "GET ": GET REQUEST
- @param successBlock :A block object to be executed when the request operation finishes successfully. This block has no return value and takes two arguments: the created request operation and the object created from the response data of request.
- @param failBlock :A block object to be executed when the request operation finishes unsuccessfully, or that finishes successfully, but encountered an error while parsing the response data. This block has no return value and takes two arguments:, the created request operation and the NSError object describing the network or parsing error that occurred.
-  @author:
- */
 
 +(void)postgetAFHTTPOperation:(NSString *)fileURL parameters:(NSDictionary*)parameters
                          isPOST:(BOOL *)isPOST
@@ -151,19 +109,6 @@
 }
 
 #pragma mark - AFURLSessionManager
-/** CREATING A DOWNLOAD TASK
- @param completionHandler:^(NSURLResponse *response, NSURL *filePath, NSError *error) {}
- @param destination:^NSURL *(NSURL *targetPath, NSURLResponse *response) {} 
-        destination A block object to be executed in order to determine the destination of the downloaded file. This block takes two arguments, the target path & the server response, and returns the desired file URL of the resulting download. The temporary file used during the download will be automatically deleted after being moved to the returned URL.
- @param progress:(NSProgress *__autoreleasing *) 
-        progress A progress object monitoring the current download progress.
- @param successBlock:
-    completionHandler A block to be executed when a task finishes. This block has no return value and takes three arguments: the server response, the path of the downloaded file, and the error describing the network or parsing error that occurred, if any.
-
- @param isResume: if isResume == Yes call run download task, else run create download task with resume data
- @warning If using a background `NSURLSessionConfiguration` on iOS, these blocks will be lost when the app is terminated. Background sessions may prefer to use `-setDownloadTaskDidFinishDownloadingBlock:` to specify the URL for saving the downloaded file, rather than the destination block of this method.
- @author:
- */
 
 +(void)createAFHTTPDownloadTask:(NSURL*)downloadURL isResume:(BOOL)isResume withData:(NSData *)data destination:(id)destination success:(id)successBlock
 {
@@ -189,15 +134,6 @@
     }
 }
 
-/** CREATING AN UPLOAD TASK with the specified request for a local file
- 
-@param uploadURL A URL to the local file to be uploaded.
-@param successBlock : ^(NSURLResponse *response, id responseObject, NSError *error) {}
-        A block object to be executed when the task finishes. This block has no return value and takes three arguments: the server response, the response object created by that serializer, and the error that occurred, if any.
-@param progress :(NSProgress *__autoreleasing *)
-        progress A progress object monitoring the current upload progress.
-@author:
- */
 
 + (void)createAFHTTPUploadTask:(NSURL*)uploadURL filePath:(NSURL*)filePath destination:(id)destination success:(id)successBlock
 {
@@ -212,14 +148,6 @@
     [uploadTask resume];
 }
 
-/** CREATING AN UPLOAD TASK FOR A MULTI-PART REQUEST, WITH PROGRESS
- @param successBlock : ^(NSURLResponse *response, id responseObject, NSError *error) {}
-        A block object to be executed when the task finishes. This block has no return value and takes three arguments: the server response, the response object created by that serializer, and the error that occurred, if any.
- @param progress :(NSProgress *__autoreleasing *) 
-        A progress object monitoring the current upload progress.
-
- @author:
- */
 
 + (void)createMultiAFHTTPUploadTask:(NSString*)uploadURL filePath:(NSString*)filePath constructingBodyWithBlock:(id)constructingBody success:(id)successBlock
 {
@@ -234,12 +162,7 @@
     [uploadTask resume];
 }
 
-/** CREATING A DATA TASK
- @param successBlock : ^(NSURLResponse *response, id responseObject, NSError *error) {}
-        completionHandler A block object to be executed when the task finishes. This block has no return value and takes three arguments: the server response, the response object created by that serializer, and the error that occurred, if any.
- @param progress :(NSProgress *__autoreleasing *)
- @author:
- */
+
 + (void)createDataAFHTTPTask:(NSURL*)uploadURL filePath:(NSString*)filePath success:(id)successBlock
 {
     
@@ -266,9 +189,8 @@
 /*
  AFNetworkReachabilityManager monitors the reachability of domains, and addresses for both WWAN and WiFi network interfaces.
 */
-/** SHARED NETWORK REACHABILITY
- 
- */
+
+
 +(void)getShareNetworkStatus{
     [[AFNetworkReachabilityManager sharedManager] setReachabilityStatusChangeBlock:^(AFNetworkReachabilityStatus status) {
         NSLog(@"Reachability: %@", AFStringFromNetworkReachabilityStatus(status));
@@ -292,5 +214,49 @@
                 break;
         }
     }];
+}
+#pragma mark - Network Reachability Manager
+
++(void)allowInvlaidSSL {
+    AFHTTPRequestOperationManager *manager = [AFHTTPRequestOperationManager manager];
+    manager.securityPolicy.allowInvalidCertificates = YES; // not recommended for production
+}
+
+#pragma mark - AFHTTPRequestOperation
+/*
+ AFHTTPRequestOperation is a subclass of AFURLConnectionOperation for requests using the HTTP or HTTPS protocols. It encapsulates the concept of acceptable status codes and content types, which determine the success or failure of a request.
+ Although AFHTTPRequestOperationManager is usually the best way to go about making requests, AFHTTPRequestOperation can be used by itself.
+ 
+ */
+
++(void)getAFHTTPRequestOperation:(NSURL*)url success:(id)successBlock fail:(id)failBlock
+{
+    NSURLRequest *request = [NSURLRequest requestWithURL:url];
+    AFHTTPRequestOperation *op = [[AFHTTPRequestOperation alloc] initWithRequest:request];
+    op.responseSerializer = [AFJSONResponseSerializer serializer];
+    [op setCompletionBlockWithSuccess:successBlock failure:failBlock];
+    [[NSOperationQueue mainQueue] addOperation:op];
+}
+
+
++(void)callBatchOfRequestOperations:(NSString*)url method:(NSString*)method batch:(id)batch filesToUpload:(NSArray *)filesToUpload success:(id)successBlock fail:(id)failBlock
+{
+    NSMutableArray *mutableOperations = [NSMutableArray array];
+    for (NSURL *fileURL in filesToUpload) {
+        NSURLRequest *request = [[AFHTTPRequestSerializer serializer] multipartFormRequestWithMethod:method URLString:url parameters:nil constructingBodyWithBlock:^(id<AFMultipartFormData> formData) {
+            [formData appendPartWithFileURL:fileURL name:@"images[]" error:nil];
+        } error:nil];
+        
+        AFHTTPRequestOperation *operation = [[AFHTTPRequestOperation alloc] initWithRequest:request];
+        
+        [mutableOperations addObject:operation];
+    }
+    
+    NSArray *operations = [AFURLConnectionOperation batchOfRequestOperations:batch progressBlock:^(NSUInteger numberOfFinishedOperations, NSUInteger totalNumberOfOperations) {
+        NSLog(@"%lu of %lu complete", (unsigned long)numberOfFinishedOperations, (unsigned long)totalNumberOfOperations);
+    } completionBlock:^(NSArray *operations) {
+        NSLog(@"All operations in batch complete");
+    }];
+    [[NSOperationQueue mainQueue] addOperations:operations waitUntilFinished:NO];
 }
 @end
